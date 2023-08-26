62544ff330c0ddff5597e3f94dd7f26ea8709977
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-08-26 18:28:15 -0400

    Add router and run server.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+async fn home() -> Html<&'static str> {
+    Html("hello world")
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route(
+            "/",
+            get(home),
+        );
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
