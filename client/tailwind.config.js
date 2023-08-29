7abaa774601390fde2d7b824196727ea24eed62b
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-08-29 09:04:11 -0400

    Update tailwind config to look at templates.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   presets: [require('tailwindcss/defaultConfig'), require('xtendui/tailwind.preset')],
-  content: ['./node_modules/xtendui/src/*.mjs', './src/**/*.{html,js}'],
+  content: ['./node_modules/xtendui/src/*.mjs', './src/**/*.{html,js}', '../templates/**/*.{html,js}'],
   theme: {
     extend: {
       // custom xtendui color
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,14 @@ import { babel } from '@rollup/plugin-babel'
 
 export default {
   root: './src',
+  publicDir: './images',
   build: {
     manifest: true,
-    //outDir: '../public',
-    rollupOptions: {
+     rollupOptions: {
       // overwrite default .html entry
       input: './src/layout.html',
     },
+    //outDir: '../public',
     outDir: '../../templates/_CLIENT_',
     emptyOutDir: true,
   },
