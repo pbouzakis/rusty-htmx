693efd980c47f1083efa0f1e8fca52121bd2b095
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-08-29 10:14:26 -0400

    Add price and update styles for shop page.

--- src/shop.rs
+++ src/shop.rs
@@ -5,6 +5,7 @@ pub struct Product {
     slug: String,
     display: String,
     image_src: String,
+    price: f32,
 }
 
 pub fn fetch_catalog() -> Vec<Product> {
@@ -13,26 +14,31 @@ pub fn fetch_catalog() -> Vec<Product> {
             slug: "the-dream-machine".into(),
             display: "The Dream Machine".into(),
             image_src: "/images/covers/dream-machine.png".into(),
+            price: 25.00,
         },
         Product {
             slug: "the-conquest-of-bread".into(),
             display: "The Conquest of Bread".into(),
             image_src: "/images/covers/bread.png".into(),
+            price: 5.00,
         },
         Product {
             slug: "hyper-media-systems".into(),
             display: "Hypermedia Systems".into(),
             image_src: "/images/covers/hypermedia.png".into(),
+            price: 10.99,
         },
         Product {
             slug: "thinking-in-systems".into(),
             display: "Thinking in Systems".into(),
             image_src: "/images/covers/systems.png".into(),
+            price: 35.50,
         },
         Product  {
             slug: "extreme-programming-explained".into(),
             display: "Extreme Programming Explained".into(),
             image_src: "/images/covers/xp.png".into(),
+            price: 20.00,
         },        
     ]
 }
