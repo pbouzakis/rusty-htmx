7d1f144bb8604520a4dce1d033431b9678747b40
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2023-09-15 15:58:10 -0400

    Add ShoppingCart model.

--- src/shop/model.rs
+++ src/shop/model.rs
@@ -1,6 +1,6 @@
 use serde::Serialize;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Product {
     pub slug: String,
     pub display: String,
@@ -12,9 +12,120 @@ pub struct Cart {
     pub items: Vec<String>,
 }
 
+pub struct ShoppingCart {
+    items: Vec<CartItem>,
+}
+
+impl ShoppingCart {
+    pub fn count(&self) -> usize {
+        self.items.len()
+    }
+
+    pub fn add(&mut self, product: &Product) -> usize {
+        if let Some(i) = self.position_of_slug(&product.slug) {
+            let cart_item = &self.items[i];
+            let quantity = cart_item.quantity + 1;
+
+            self.items[i] = CartItem {
+                product: product.clone(),
+                quantity,
+                total: product.price * quantity as f32,
+            }
+
+        } else {
+            self.items.push(CartItem {
+                product: product.clone(),
+                quantity: 1,
+                total: product.price,
+            });
+        }
+
+        self.count()
+    }
+
+    pub fn items(&self) -> &Vec<CartItem> {
+       &self.items
+    }
+
+    fn position_of_slug(&self, slug: &str)-> Option<usize> {
+        self.items.iter().position(|p| p.product.slug == slug)
+    }
+}
+
+
 #[derive(Serialize)]
 pub struct CartItem {
     pub product: Product,
     pub quantity: usize,
     pub total: f32,
+}
+
+#[cfg(test)]
+mod tests {
+    fn new_dummy_product() -> super::Product {
+        super::Product {
+            slug: "test-slug".into(),
+            display: "test display".into(),
+            image_src: "https://image".into(),
+            price: 25.99,
+        }
+    }
+
+    fn new_dummy_cart_item() -> super::CartItem {
+        super::CartItem {
+            product: new_dummy_product(),
+            quantity: 1,
+            total: 25.99,
+        }
+    }
+
+    #[test]
+    fn empty_cart() {
+        let cart = super::ShoppingCart {
+            items: Vec::new(),
+        };
+
+        assert_eq!(cart.count(), 0);
+    }
+    
+    #[test]
+    fn adding_to_cart_to_empty_cart() {
+        let mut cart = super::ShoppingCart {
+            items: Vec::new(),
+        };
+
+        cart.add(&new_dummy_product());
+
+        assert_eq!(cart.count(), 1);
+    }
+
+    #[test]
+    fn adding_to_cart_to_non_empty_cart() {
+        let mut cart = super::ShoppingCart {
+            items: vec![new_dummy_cart_item()],
+        };
+
+        let product = super::Product {
+            slug: "test-slug-two".into(),
+            display: "test display two".into(),
+            image_src: "https://image-two".into(),
+            price: 25.99,
+        };
+
+        cart.add(&product);
+
+        assert_eq!(cart.count(), 2);
+    }
+
+    #[test]
+    fn adding_the_same_sku_twice_does_not_inc_count() {
+        let mut cart = super::ShoppingCart {
+            items: vec![new_dummy_cart_item()],
+        };
+
+        cart.add(&new_dummy_product());
+
+        assert_eq!(cart.count(), 1, "Count should not increment");
+        assert_eq!(cart.items()[0].quantity, 2, "Quantiy should increment");
+    }         
 }
